41ed665a6cbb9ddc399213b785649ab01f5fd624
Manuel Burger <manuel.burger@inf.ethz.ch>
2024-04-08 10:48:43 +0200

    `death_icu` implemented

--- R/callback-icu-mortality.R
+++ R/callback-icu-mortality.R
@@ -62,18 +62,29 @@ miiv_death_icu <- function(x, env, ...){
 }
 
 picdb_death_icu <- function(x, env, ...){
+  #' Extract in-ICU mortality from PICdb
+  #' 
+  #' We rely on a concept-dict config, which passes rows for each `icustay_id`
+  #' separately (e.g. the `icustays` table). As this table does not contain mortality
+  #' information we rely on a auxiliary column defined for all patients in PICdb in
+  #' the chosen table (e.g. `los`).
+  #' We extract the last ICU stay for each `hadm_id`. We reset the `los` column to be
+  #' FALSE for each `icustay_id` and set it to TRUE for the `icustay_id` of the last
+  #' ICU stay of each `hadm_id`, which is marked as deceased.
 
-  icu_stays <- env[["icustays"]]
+  icu_stays <- load_ts(env[["icustays"]], id_var = "hadm_id", index_var = "intime", interval = mins(1L))
+  admissions <- load_id(env[["admissions"]], id_var = "hadm_id", cols = c("hospital_expire_flag"))
 
   # For each "hadm_id" we find the "icustay_id" of the last ICU stay based on the "intime"
-  last_icu_stay <- icu_stays[ , .(icustay_id = icustay_id[which.max(intime)]), by = hadm_id]
-  print(last_icu_stay)
-  print(x)
+  last_icu_stay <- icu_stays[, .(icustay_id = icustay_id[.N]), by = hadm_id]
+  deceased_hadm_id <- admissions[ricu:::is_true(hospital_expire_flag), .(hadm_id)]$hadm_id
+  deceased_icustay_id <- icu_stays[hadm_id %in% deceased_hadm_id, .(icustay_id)]$icustay_id
 
-  # icu_wards <- sort(unique(env[["icustays"]]$first_careunit))
-  # transfers <- load_ts(env[["icustays"]], id_var = "hadm_id", index_var = "intime", interval = mins(1L))
-  # transfers <- change_id(transfers, "icustay", as_src_cfg(env), id_type = TRUE)
-
-  # rename_cols(transfers, "ward", "curr_careunit", by_ref = TRUE)
-  # mi_death_icu(x, transfers, icu_wards, ...)
+  # For all `icustay_id` not in last_icu_stay, we set the value of the `hospital_expire_flag` variable to FALSE
+  # Patients only die in the last ICU stay
+  # `los` should be configured to be the target extraction column as a dummy column
+  # to put the value of the `hospital_expire_flag` variable
+  x[, los := FALSE]
+  x[icustay_id %in% deceased_icustay_id, los := TRUE]
+  x[!icustay_id %in% last_icu_stay$icustay_id, los := FALSE]
 }
